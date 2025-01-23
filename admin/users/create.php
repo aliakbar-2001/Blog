b9c1e081769bc4d9d97a5c8596c9d0363956cfa1
Ali Akbar Rakib <130457076+aliakbar-2001@users.noreply.github.com>
2025-01-23 13:19:26 +0600

    Add files via upload

---
+++ admin/users/create.php
@@ -0,0 +1,101 @@
+<?php include("../../path.php"); ?>
+<?php include(ROOT_PATH . "/app/contorollers/users.php"); 
+adminOnly();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font Awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- link style css -->
+     <link rel="stylesheet" href="../../assets/css/style.css">
+
+    <!-- link admin css -->
+    <link rel="stylesheet" href="../../assets/css/admin.css">
+
+    <!-- link google fonts -->
+     <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet" >
+    <title>Admin Section - Add User</title>
+</head>
+<body>
+    <!-- ================= Start Nav bar ==================-->
+    <?php include(ROOT_PATH . "/app/includes/adminHeader.php"); ?>
+    <!-- ================= End Nav bar ==================-->
+
+    <!-- ================= Start Admin page warper ==================-->
+    <div class="admin-warper">
+
+    <!-- Left sidebar-->
+    <?php include(ROOT_PATH . "/app/includes/adminSidebar.php"); ?>
+    <!-- Left sidebar-->
+        <!-- Admin content-->
+        <div class="admin-content">
+            <div class="button-group">
+                <a href="create.php" class="btn btn-big">Add User</a>
+                <a href="index.php" class="btn btn-big">Manage Users</a>
+            </div>
+
+            <div class="content">
+                <h2 class="page-title">Add User</h2>
+
+                <?php include(ROOT_PATH ."/app/helpers/formError.php"); ?>
+
+                <form action="create.php" method="post">
+                    <div>
+                        <label>Username</label>
+                        <input type="text" name="username" value="<?php echo $username; ?>" class="text-input">
+                    </div>
+        
+                    <div>
+                        <label>Email</label>
+                        <input type="email" name="email" value="<?php echo $email; ?>" class="text-input">
+                    </div>
+        
+                    <div>
+                        <label>Password</label>
+                        <input type="password" name="password" value="<?php echo $password; ?>" class="text-input">
+                    </div>
+        
+                    <div>
+                        <label>Password Confirmation</label>
+                        <input type="password" name="passwordConf" value="<?php echo $passwordConf; ?>" class="text-input">
+                    </div>
+
+                    <div>
+                        <?php if(isset($admin) && $admin == 1): ?>
+                            <label>
+                                <input type="checkbox" name="admin" checked>
+                                Admin
+                            </label>
+                        <?php else: ?>
+                            <label>
+                                <input type="checkbox" name="admin">
+                                Admin
+                            </label>
+                        <?php endif; ?>
+                    </div>
+
+                    <div>
+                        <button type="submit" name="create-admin" class="btn btn-big">Add User</button>
+                    </div>
+                </form>    
+            </div>
+         </div>
+        <!-- Admin content-->
+
+    </div>
+    <!-- ================= End Admin page warper ==================-->
+
+    <!-- JQuary link-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- ckeditor link-->
+    <script src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
+
+    <!-- link js-->
+     <script src="../../assets/js/script.js"></script>
+</body>
+</html>
---
+++ admin/users/index.php
@@ -0,0 +1,78 @@
+<?php include("../../path.php"); ?>
+<?php include(ROOT_PATH . "/app/contorollers/users.php"); 
+adminOnly();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font Awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- link style css -->
+     <link rel="stylesheet" href="../../assets/css/style.css">
+
+    <!-- link admin css -->
+    <link rel="stylesheet" href="../../assets/css/admin.css">
+
+    <!-- link google fonts -->
+     <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet" >
+    <title>Admin Section - Manage Users</title>
+</head>
+<body>
+    <!-- ================= Start Nav bar ==================-->
+    <?php include(ROOT_PATH . "/app/includes/adminHeader.php"); ?>
+    <!-- ================= End Nav bar ==================-->
+
+
+    <!-- ================= Start Admin page warper ==================-->
+    <div class="admin-warper">
+        <!-- Left sidebar-->
+        <?php include(ROOT_PATH . "/app/includes/adminSidebar.php"); ?>
+        <!-- Left sidebar-->
+
+        <!-- Admin content-->
+        <div class="admin-content">
+            <div class="button-group">
+                <a href="create.php" class="btn btn-big">Add User</a>
+                <a href="index.php" class="btn btn-big">Manage Users</a>
+            </div>
+
+            <div class="content">
+                <h2 class="page-title">Manage Users</h2>
+
+                <?php include(ROOT_PATH."/app/includes/messages.php"); ?> 
+
+                <table>
+                    <thead>
+                        <th>SN</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th colspan="2">Action</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach($admin_users as $key => $user): ?>
+                            <tr>
+                                <td><?php echo $key + 1 ; ?></td>
+                                <td><?php echo $user['username']; ?></td>
+                                <td><?php echo $user['email']; ?></td>
+                                <td><a href="edit.php?id=<?php echo $user['id']; ?>" class="edit">Edit</a></td>
+                                <td><a href="index.php?delete_id=<?php echo $user['id']; ?>" class="delete">Delete</a></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+         </div>
+        <!-- Admin content-->
+    </div>
+    <!-- ================= End Admin page warper ==================-->
+
+    <!-- JQuary link-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- link js-->
+     <script src="../../assets/js/script.js"></script>
+</body>
+</html>
